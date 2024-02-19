b5b978a5144b4fc7a241cb4f9fe592b70f82b3bb
Artuur Alexandrov <naturahxq@gmail.com>
2024-02-19 17:35:21 +0300

    github search

--- src/pages/FavouritesPage/FavouritesPage.tsx
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,9 +1,19 @@
+import {useAppSelector} from '../../hooks/redux'
 
+function FavouritesPage() {
+  const {favourites} = useAppSelector(state => state.github)
 
-const FavouritesPage = () => {
+  if (favourites.length === 0) return <p className="text-center">No items.</p>
+
   return (
-    <div>
-      FavouritesPage
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        { favourites.map(f => (
+          <li key={f}>
+            <a href={f} target="_blank">{f}</a>
+          </li>
+        )) }
+      </ul>
     </div>
   )
 }
